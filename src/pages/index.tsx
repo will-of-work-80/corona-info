7cf262de05cd91a7e561a664608b407c4636e95c
will-of-work-80 <will.of.work.80@gmail.com>
2026-07-13 19:19:20 +0900

    コメントを修正しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const TopRoot: FunctionComponent<Props> = (props) => {
                 />
             ) : (
                 <>
-                    <div>エラーが発生しました。以下をご確認ください。</div>
+                    <div>エラーが発生しました。以下をご確認</div>
                     <div>{props.errorInfo.errorCode}</div>
                     <div>{props.errorInfo.errorMessage}</div>
                 </>
